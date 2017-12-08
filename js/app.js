ade00003eff3817ad40ac3b1651b45674c79497d
rickytranmer <rwtranmer@gmail.com>
2017-12-08 15:57:46 -0700

    adds finish line and win condition

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
-//----------Vanilla JS ONLY	----------
+//----------Vanilla JS ONLY	----------//
 var player1Left = 0;
 var player2Left = 0;
+var stillRacing = true;
+
+//ADD - Player objects {playerNjame, leftMargin, score}
 
 const body = document.querySelector('body');
 const theHead = document.querySelector('header');
@@ -8,46 +11,67 @@ const defMargin = '25vh 0 25vh ';
 const player1 = document.getElementById('player1');
 const player2 = document.getElementById('player2');
 
-//----------				----------
-//ADD - Change 'ready?' to 'go!' after timeout
+
+//----------				----------//
+// - 'ready?' header - //
 var readyGo = document.createElement('h1');
 readyGo.innerHTML = 'ready?';
+readyGo.style.color = 'red';
 theHead.appendChild(readyGo);
+
+//----------				----------//
+// - Create finish line - //
+var finishLine = document.createElement('div');
+finishLine.style.float = 'right';
+finishLine.style.borderRight = '6px solid red';
+finishLine.style.height = '85vh';
+finishLine.style.marginTop = '-85vh';
+body.appendChild(finishLine);
+
+//----------				----------//
+// - 'GO!' header - //
 setTimeout( function() {
 	readyGo.innerHTML = 'GO!';
+	readyGo.style.color = 'green';
+	finishLine.style.borderRight = '6px solid green';
 
 	// - Add event listener for keyup, hone in on 'F' & 'J' for player inputs
 	document.addEventListener('keyup', function(event) {
 	  const keyName = event.key;
 	  console.log(keyName + ' pressed');
-	  if (keyName === 'f') {	
-	  	console.log(player1);
+	  if ((stillRacing) && (keyName === 'f')) {	
+	  	console.log('player1:');
 	  	movePlayer1();
-	  } else if (keyName === 'j') {	
+	  } else if ((stillRacing) && (keyName === 'j')) {	
+	  	console.log('player2:');
 	  	console.log(player2);
 	  	movePlayer2();
 	  }
 	});
+
+	//ADD - Add listener for click on squares (alternate movement method)
 }, 2000);
 
-//ADD - Create finish line
 
-
-//ADD - Add listener for click on squares (alternate movement method)
-
-
-//ADD - Move player
+//----------				----------//
+// - Move players - //
 function movePlayer1 () {
 	player1Left += 5;
 	player1.style.margin = defMargin + player1Left + 'vw';
 
-	//ADD - Check finish line
+	// - Check finish line - //
+	if (player1Left >= 90) {
+		stillRacing = false;
+		setTimeout( function() { alert('PLAYER 1 WINS') }, 125);
+	}
 }
-
-//ADD - Move player
 function movePlayer2 () {
 	player2Left += 5;
 	player2.style.margin = defMargin + player2Left + 'vw';
 
-	//ADD - Check finish line
+	// - Check finish line (>=90vw) - //
+	if (player2Left >= 90) {
+		stillRacing = false;
+		setTimeout( function() { alert('PLAYER 2 WINS') }, 125);
+	}
 }
