e850dddeb16fa2bf4c74264b159708b702cdf2a3
rickytranmer <rwtranmer@gmail.com>
2017-12-08 10:42:23 -0700

    moves player1 if 'f' pressed

--- js/app.js
+++ js/app.js
@@ -1,11 +1,31 @@
 //Vanilla JS ONLY
+var player1Left = 0;
+var player2Left = 0;
+const defMargin = '25vh 0 25vh ';
 
 //ADD - Create finish line
 
-//ADD - Add event listeners two buttons
+//ADD - Add event listeners for two buttons
 document.addEventListener('keyup', function(event) {
   const keyName = event.key;
-  console.log(keyName + ' pressed')
+  console.log(keyName + ' pressed');
+  if (keyName === 'f') {
+  	var player1 = document.getElementById('player1');
+  	player1Left += 5;
+  	player1.style.margin = defMargin + player1Left + 'vw';
+  	console.log(keyName);	
+  	console.log(player1);
+  }
 });
 
+
+
 //ADD - Move player
+function movePlayer1 () {
+	//ADD - Check finish line
+}
+
+//ADD - Move player
+function movePlayer2 () {
+	//ADD - Check finish line
+}
