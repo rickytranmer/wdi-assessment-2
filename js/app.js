5cfe23432aca4b0343bf322a0f087466a5d72a2a
rickytranmer <rwtranmer@gmail.com>
2017-12-08 09:59:21 -0700

    take player input, link to game page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 //Vanilla JS ONLY
+
+//ADD - Create finish line
+
+//ADD - Add event listeners two buttons
+document.addEventListener('keyup', function(event) {
+  const keyName = event.key;
+  console.log(keyName + ' pressed')
+});
+
+//ADD - Move player
