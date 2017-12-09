131daeb9dbb74dc1b20105564ba3d0463dd853d7
rickytranmer <rwtranmer@gmail.com>
2017-12-08 23:31:20 -0700

    replaces several variables with two player objects

--- js/app.js
+++ js/app.js
@@ -1,32 +1,44 @@
 //----------Vanilla JS ONLY	----------//
-var player1Left = 0;
-var player2Left = 0;
+var player1 = {
+	playerName: 'player1',
+	leftMargin: 0,
+	score: 0,
+	divBox: document.getElementById('player1')
+};
+var player2 = {
+	playerName: 'player2',
+	leftMargin: 0,
+	score: 0,
+	divBox: document.getElementById('player2')
+};
 var stillRacing = true;
 
-//ADD - Player objects {playerNjame, leftMargin, score}
+console.log(player1);
+console.log(player2);
 
 const body = document.querySelector('body');
 const theHead = document.querySelector('header');
 const defMargin = '25vh 0 25vh ';
-const player1 = document.getElementById('player1');
-const player2 = document.getElementById('player2');
 
-
-//----------				----------//
-// - 'ready?' header - //
-var readyGo = document.createElement('h1');
-readyGo.innerHTML = 'ready?';
-readyGo.style.color = 'red';
-theHead.appendChild(readyGo);
 
 //----------				----------//
 // - Create finish line - //
 var finishLine = document.createElement('div');
 finishLine.style.float = 'right';
 finishLine.style.borderRight = '6px solid red';
-finishLine.style.height = '85vh';
+finishLine.style.height = '75vh';
 finishLine.style.marginTop = '-85vh';
 body.appendChild(finishLine);
+
+
+//----------				----------//
+// - 'ready?' header & players - //
+var readyGo = document.createElement('h1');
+readyGo.innerHTML = 'ready?';
+readyGo.style.color = 'red';
+theHead.appendChild(readyGo);
+player1.divBox = document.getElementById('player1');
+player2.divBox = document.getElementById('player2');
 
 //----------				----------//
 // - 'GO!' header - //
@@ -41,6 +53,7 @@ setTimeout( function() {
 	  console.log(keyName + ' pressed');
 	  if ((stillRacing) && (keyName === 'f')) {	
 	  	console.log('player1:');
+	  	console.log(player1);
 	  	movePlayer1();
 	  } else if ((stillRacing) && (keyName === 'j')) {	
 	  	console.log('player2:');
@@ -56,22 +69,24 @@ setTimeout( function() {
 //----------				----------//
 // - Move players - //
 function movePlayer1 () {
-	player1Left += 5;
-	player1.style.margin = defMargin + player1Left + 'vw';
+	player1.leftMargin += 5;
+	player1.divBox.style.margin = defMargin + player1.leftMargin + 'vw';
 
 	// - Check finish line - //
-	if (player1Left >= 90) {
+	if (player1.leftMargin >= 90) {
 		stillRacing = false;
-		setTimeout( function() { alert('PLAYER 1 WINS') }, 125);
+		readyGo.innerHTML = 'PLAYER 1 WINS';
+		body.style.backgroundColor = 'pink';
 	}
 }
 function movePlayer2 () {
-	player2Left += 5;
-	player2.style.margin = defMargin + player2Left + 'vw';
+	player2.leftMargin += 5;
+	player2.divBox.style.margin = defMargin + player2.leftMargin + 'vw';
 
 	// - Check finish line (>=90vw) - //
-	if (player2Left >= 90) {
+	if (player2.leftMargin >= 90) {
 		stillRacing = false;
-		setTimeout( function() { alert('PLAYER 2 WINS') }, 125);
+		readyGo.innerHTML = 'PLAYER 2 WINS';
+		body.style.backgroundColor = 'lightBlue';
 	}
 }
